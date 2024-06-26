7b940ab22ade513687876f54fcf76c54608c5b8f
LalithGhatty <gvsklalith2005@gmail.com>
2024-06-26 12:42:15 +0530

    Double Linked List

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class DoublyLinkedList {
     static class Node {
         int data;
         Node next;
@@ -14,29 +14,52 @@ public class Main {
 
     Node head = null;
     Node tail = null;
-    public void create(){
-        Scanner sc  = new Scanner(System.in);
-        int data,m,n,p;
-        do{
+    public void create() {
+        Scanner sc = new Scanner(System.in);
+        int data, m, n, p;
+        do {
             System.out.println("Enter the data");
             data = sc.nextInt();
             Node newNode = new Node(data);
-            if(head == null){
-                head  = newNode;
+            if (head == null) {
+                head = newNode;
                 tail = newNode;
                 System.out.println("Doubly Linked List created");
-            }
-            else{
-                head.prev = newNode;
-                newNode.next = head;
-                head = newNode;
-                System.out.println("Node added at the beginning of the Doubly linked list ");
-            }
-          System.out.println("Enter 1 to insert more data");
+            } else {
+                System.out.println("Enter 1 to insert the node at the beginning,2 to insert the node at the end, 3 to insert the node at the specific position");
+                m = sc.nextInt();
+                switch (m) {
+                    case 1:
+                        newNode.next = head;
+                        head.prev = newNode;
+                        head = newNode;
+                        System.out.println("Node added at the beginning of the Doubly Linked List");
+                        break;
+                    case 2:
+                        tail.next = newNode;
+                        newNode.prev = tail;
+                        tail = newNode;
+                        break;
+                    case 3:
+                        System.out.println("Enter the position where you want to insert the data");
+                        p = sc.nextInt();
+                        Node temp = head;
+                        Node ptr = temp.next;
+                        for (int i = 1; i < p - 1; i++) {
+                            temp = ptr;
+                            ptr = ptr.next;
+                        }
+                        temp.next = newNode;
+                        newNode.prev = temp;
+                        ptr.prev = newNode;
+                        newNode.next = ptr;
+                        System.out.println("Node added at the " + "" + p + "" + "th" + "position");
+                        break;
+                }
+            } System.out.println("Enter 1 to insert more data");
             n = sc.nextInt();
         }
-        while(n == 1);
-    }
+        while(n == 1);}
     public void traverse(){
         Node temp = head;
         if(head == null){
@@ -53,7 +76,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Main obj = new Main();
+        DoublyLinkedList obj = new DoublyLinkedList();
         obj.create();
         obj.traverse();
 
