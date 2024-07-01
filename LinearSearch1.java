52d150b23d155584a7d40802664d3199350a6409
LalithGhatty <gvsklalith2005@gmail.com>
2024-07-01 18:51:55 +0530

    Linear Search using Random class in java

---
+++ LinearSearch1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class LinearSearch1 {
+    public static void main(String[] args) {
+        Random random  = new Random();
+        System.out.println("Enter the length of the array");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arr = new int[len];
+        for(int i = 0;i<len;i++){
+            arr[i] = random.nextInt(10000);
+        };
+        System.out.println("Enter the number you want to search in the array");
+        int required = sc.nextInt();
+        LinearSearch(arr,required);
+    }
+    public static int LinearSearch(int[] array,int target){
+        int count = 0;
+        for(int i = 0 ;i<array.length;i++){
+            if(array[i] == target){
+               System.out.println(array[i]);
+               System.out.println("Match found at :" + i + "th  position");
+               count++;
+            }
+        }
+        System.out.println("Number of times required number is found in the array is:" + " " + count);
+        return -1;
+    }
+}
